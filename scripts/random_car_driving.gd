984c0ed6b6f2ca2d723f95ad344a1a8e44ceb7bd
Emil Wraae Carlsen <s204458@student.dtu.dk>
2025-02-20 12:10:52 +0100

    Tester deleted and car avoidance added

--- scripts/random_car_driving.gd
+++ scripts/random_car_driving.gd
@@ -9,7 +9,6 @@ var speed = 100
 func _ready() -> void:
 	sprite.frame = randi_range(0,41)
 	make_path()
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _physics_process(delta: float) -> void:
 #	var dir = to_local(nav_agent.target_position).normalized()
@@ -21,8 +20,10 @@ func _physics_process(_delta: float) -> void:
 		speed = 0
 	var next_path_pos := nav_agent.get_next_path_position()
 	var dir := global_position.direction_to(next_path_pos)
-	velocity = dir * speed
+	var intended_velocity = dir * speed
 	car.rotation = dir.angle() + (PI/2.0)
+	nav_agent.set_velocity(intended_velocity)
+	
 	move_and_slide()
 
 
@@ -30,4 +31,9 @@ func make_path() -> void:
 	nav_agent.target_position = parkingSpot.global_position
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	make_path()
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+	move_and_slide()
