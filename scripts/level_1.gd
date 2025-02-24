d904163c652ccee7005295d7bd79be99bbfc1fd8
Emil Wraae Carlsen <s204458@student.dtu.dk>
2025-02-24 14:50:31 +0100

    GUI

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,56 +1,69 @@
 extends Node2D
 @onready var cars: Node = $Cars
 @onready var parking: Node = $Parking
-@onready var helper: Label = $Labels/Helper
+@onready var helper: Label = $GUI/ingameGUI/Helper
+@onready var textbox: Label = $GUI/Textbox/Label
 
+var nrCars
 var carIndex = 0
 var currentCar
 var nextCar
-var park = 0
+var upPark = 0
+var downPark = 0
 var done = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	nrCars = cars.get_child_count()
 	currentCar = cars.get_child(carIndex) 
+	textbox.text = "Hvis bilen er rød
+Så skal den parkere øverst
+Ellers skal den parkere nederst"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if done > 4:
+	if done > nrCars-1:
 		helper.text = "Godt gået!"
-		helper.modulate = "e8b705"
+		helper.modulate = "00ffff"
 
 
 
 
 
-func _on_up_button_pressed() -> void:
+func _on_up_pressed() -> void:
 	if currentCar.color >= 7:
 		rigtigt(0)
 	else:
 		forkert()
 
-func _on_down_button_pressed() -> void:
+func _on_down_pressed() -> void:
 	if currentCar.color <= 6:
 		rigtigt(1)
 	else:
 		forkert()
 
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
+
+#Helper functions
 func rigtigt(x: int) -> void:
 	helper.text = "Rigtigt"
 	helper.modulate = "07d434"
 	done += 1
-	currentCar.parkingSpot = parking.get_child(x).get_child(park)
-	if carIndex < 4:
+	if x == 0:
+		currentCar.parkingSpot = parking.get_child(x).get_child(upPark)
+	else:
+		currentCar.parkingSpot = parking.get_child(x).get_child(downPark)
+	if carIndex < nrCars-1:
 		carIndex += 1
-		park += 1
+		if x == 0:
+			upPark += 1
+		else:
+			downPark += 1
 		currentCar = cars.get_child(carIndex)
 		currentCar.parkingSpot = parking.get_child(2)
 
 func forkert() -> void:
 	helper.text = "FORKERT"
 	helper.modulate = "da2031"
-
-
-func _on_restart_button_pressed() -> void:
-	get_tree().reload_current_scene()
