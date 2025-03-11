557470a460fcd114a6c7a2295563286a65963d2a
Emil Wraae Carlsen <s204458@student.dtu.dk>
2025-03-11 10:22:03 +0100

    Arrow keys work now

--- scripts/button_box.gd
+++ scripts/button_box.gd
@@ -16,18 +16,27 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	helper.text = owner.owner.helper
 
+	if Input.is_action_just_pressed("ui_up") && !up.disabled:
+		_on_up_pressed()
+	if Input.is_action_just_pressed("ui_left") && !left.disabled:
+		_on_left_pressed()
+	if Input.is_action_just_pressed("ui_right") && !right.disabled:
+		_on_right_pressed()
+	if Input.is_action_just_pressed("ui_down") && !down.disabled:
+		_on_down_pressed()
+
 
 func _on_up_pressed() -> void:
 	owner.owner._on_up_pressed()
 
-
 func _on_left_pressed() -> void:
 	owner.owner._on_left_pressed()
-
 
 func _on_right_pressed() -> void:
 	owner.owner._on_right_pressed()
 
-
 func _on_down_pressed() -> void:
 	owner.owner._on_down_pressed()
+
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
