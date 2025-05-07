c06cc37eb2e21301f87856904f2d89c2fb92f1ad
Emil Wraae Carlsen <s204458@student.dtu.dk>
2025-05-07 22:02:38 +0200

    2-10 & 2-11 added
    Small text fix in 2-08

---
+++ scripts/levels/2-10.gd
@@ -0,0 +1,62 @@
+extends BaseDropdownLevel
+
+@onready var intro: Control = $GUI/IngameGUIDropdown/Intro
+
+# Called when the node enters the scene tree for the first time.
+func setup() -> void:
+	# Level setup
+	level = 16
+	
+	# Car options
+	usedColors = [3, 4]
+	carColors = [[3],[3],[3],[4],[4],[4],[3, 4],[3, 4],[3, 4]] #0=Blue 1=Red 2=Orange 3=Purple 4=Green 5=Yellow
+	carOrigins = [[0],[0],[0],[0],[3],[3],[3],[0, 3],[0, 3]] #0=Up 1=Left 2=Right 3=Down
+	carShapes = [0]
+	
+	# Win conditions
+	leftCond = ["4_3_0", "4_0_0"]
+	rightCond = ["3_3_0", "3_0_0"]
+	assign_conditions()
+	
+	# Select if dropdowns to use
+	ifArray.push_back("colorIf")
+	ifLabel.push_back("Hvis bilen er ")
+	thenLabel.push_back("så skal den parkere til højre.")
+	
+	ifArray.push_back("colorIf")
+	ifLabel.push_back("Hvis bilen er ")
+	thenLabel.push_back("så skal den parkere til venstre.")
+	
+	# Text
+	tips.push_back("Tryk på en dropdown for at se de mulige farver og fordel dem så det passer med farverne på pladserne.")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Logging 
+	stopwatch.update(delta)
+	
+	disableRun = checkRun()
+	
+	completeLevel()
+
+func _on_run_pressed() -> void:
+	if canRun:
+		runCounter
+		canRun = false
+		for car in nrCars:
+			if await wait(1):
+				spawnCar()
+				if currentCar.color == optionSelected[0]:
+					if !moveCar(2): # Right
+						pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
+						break
+				elif currentCar.color == optionSelected[1]:
+					if !moveCar(1): # Left
+						pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
+						break
+				else:
+					pop_up_complete.lose("Du har ikke bestemt hvor denne farve bil skal hen.")
+					break
+			else:
+				break
--- scripts/levels/2-11.gd
+++ scripts/levels/2-11.gd
@@ -8,39 +8,34 @@ func setup() -> void:
 	
 	# Car options
 	usedColors = [0, 1, 3, 4, 5]
-	carColors = [[1],[1],[1,4],[4],[4],[3],[3],[3],[0],[5],[0,5],[0,5],[0,5],[0,5],[0,5]] #0=Blue 1=Red 2=Orange 3=Purple 4=Green 5=Yellow
-	carOrigins = [[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1]] #0=Up 1=Left 2=Right 3=Down
+	carColors = [[0],[0],[0],[0],[1],[1],[1,3],[3],[3],[4],[4],[4, 5],[5],[5]] #0=Blue 1=Red 2=Orange 3=Purple 4=Green 5=Yellow
+	carOrigins = [[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1]] #0=Up 1=Left 2=Right 3=Down
 	carShapes = [0]
 	
 	# Win conditions
-	upCond = ["0_1_0","5_1_0"]
-	rightCond = ["1_1_0","4_1_0"]
-	downCond = ["0_1_0","3_1_0","5_1_0"]
+	upCond = ["0_1_0"]
+	rightCond = ["4_1_0","5_1_0"]
+	downCond = ["1_1_0","3_1_0"]
 		
 	# Select if dropdowns to use
-	ifArray.push_back("colorIf")
-	ifLabel.push_back("Hvis bilen er ")
-	thenLabel.push_back("så skal den parkere til højre.")
-	
 	ifArray.push_back("colorIf")
 	ifLabel.push_back("Hvis bilen er ")
 	thenLabel.push_back("så skal den parkere nedad.")
 	
 	ifArray.push_back("colorIf")
 	ifLabel.push_back("Hvis bilen er ")
-	thenLabel.push_back("så skal den parkere til højre.")
+	thenLabel.push_back("så skal den parkere opad.")
+	
+	ifArray.push_back("colorIf")
+	ifLabel.push_back("Hvis bilen er ")
+	thenLabel.push_back("så skal den parkere nedad.")
 	
 	elseVisible = true
 	elseLabel = "Ellers må den parkere 
-					på de pladser der ikke er reserverede."
+					til højre"
 	
 	# Text
-	textbox = "Højre side er reserveret til røde og grønne biler,
-						lilla biler skal parkere i bunden
-						og andre farver må parkere på de plader der ikke er reserverede."
-	tips.push_back("Der er forskel på, om en plads er reserveret, eller om en bil skal parkere et bestemt sted.")
-	tips.push_back("Rækkefølgen af reglerne er ikke nødvendigvis den samme som rækkefølgen i din instruktion.")
-	tips.push_back(textbox)
+	tips.push_back("Her kommer der 5 forskelige farver biler, men du kan kun vælge 3 bestemte farver. De sidste 2 skal bruge \"ellers\" udtrykket")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -59,21 +54,20 @@ func _on_run_pressed() -> void:
 			if await wait(1):
 				spawnCar()
 				if currentCar.color == optionSelected[0]:
-					if !moveCar(2): # Right
-						pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
-						break
-				elif currentCar.color == optionSelected[1]:
 					if !moveCar(3): # Down
 						pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
 						break
+				elif currentCar.color == optionSelected[1]:
+					if !moveCar(0): # Up
+						pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
+						break
 				elif currentCar.color == optionSelected[2]:
-					if !moveCar(2): # Right
+					if !moveCar(3): # Down
 						pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
 						break
 				else:
-					if !moveCar(0):
-						if !moveCar(3):
-							pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
-							break
+					if !moveCar(2): # Right
+						pop_up_complete.lose("Der er ikke plads til bilen hvis den skal følge dine instruktioner.")
+						break
 			else:
 				break
